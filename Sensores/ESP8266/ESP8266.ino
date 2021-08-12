241de4183eb89590fdd44683404e8ea266fc5679
clairgon <clairgon@espol.edu.ec>
2021-08-11 20:49:33 -0500

    Conexión a base de datos

--- Sensores/ESP8266/ESP8266.ino
+++ Sensores/ESP8266/ESP8266.ino
@@ -1,11 +1,16 @@
 //ESP8266
 #include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
+#include <FirebaseESP8266.h>
+#define FIREBASE_HOST "pruebas-782c9-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "kUM0iAZER1fY3T5Q3T0JWGdzXKbDKG3mdtw37nvK"
 
 ESP8266WebServer server;
-
+WiFiClient client;
+FirebaseData firebaseData;
 const char* ssid = "NETLIFE-BARATAU";
 const char* contraseña = "Antonio68";
+
 void setup() {
   // put your setup code here, to run once:
   WiFi.begin(ssid,contraseña);
@@ -18,9 +23,12 @@ void setup() {
   Serial.print(" ");
   Serial.print("IP: ");
   Serial.println(WiFi.localIP());
+
+  Firebase.begin(FIREBASE_HOST,FIREBASE_AUTH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Firebase.setString(firebaseData,"temperatura",String("30"));
+  Firebase.setString(firebaseData,"humedad",String("1"));
 }
