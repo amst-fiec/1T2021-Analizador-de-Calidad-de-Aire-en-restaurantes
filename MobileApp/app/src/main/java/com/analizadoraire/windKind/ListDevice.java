81956774ec6bd40cf57057700353c3ce1dc67bf6
Ronaldo Rodriguez <clerodri@espol.edu.ec>
2021-08-12 17:47:12 -0500

    Vista_Dispositivos

--- MobileApp/app/src/main/java/com/analizadoraire/windKind/ListDevice.java
+++ MobileApp/app/src/main/java/com/analizadoraire/windKind/ListDevice.java
@@ -3,12 +3,28 @@ package com.analizadoraire.windKind;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ListDevice extends AppCompatActivity {
+    private TextView listDevice;
+    private ListView list_v1;
+    private String nombre_dispositivo[]={};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_device);
+        listDevice =  findViewById(R.id.listDevice);
+        list_v1 = findViewById(R.id.list_v1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,R.layout.list_view_devices,nombre_dispositivo);
+        list_v1.setAdapter(adapter);
+
+
+
     }
+
+
 }
