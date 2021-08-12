249a9d14682a56ac2244b27f7e42dbcf9ca39bcc
clairgon <clairgon@espol.edu.ec>
2021-08-11 23:22:02 -0500

    Envío de temperatura y humedad a base de datos

--- Sensores/ESP8266/ESP8266.ino
+++ Sensores/ESP8266/ESP8266.ino
@@ -4,6 +4,9 @@
 #include <FirebaseESP8266.h>
 #define FIREBASE_HOST "pruebas-782c9-default-rtdb.firebaseio.com"
 #define FIREBASE_AUTH "kUM0iAZER1fY3T5Q3T0JWGdzXKbDKG3mdtw37nvK"
+#include "DHT.h"
+#define DHTTYPE DHT11
+#define DHTpin 14
 
 ESP8266WebServer server;
 WiFiClient client;
@@ -14,8 +17,10 @@ float temperatura = 0;
 float humedad = 0;
 float calidad = 0;
 
+DHT dht(DHTpin,DHTTYPE);//iniciar dht11
+
 void setup() {
-  // put your setup code here, to run once:
+  dht.begin();
   WiFi.begin(ssid,contraseña);
   Serial.begin(9600);
   while (WiFi.status()!=WL_CONNECTED){
@@ -31,8 +36,11 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  temperatura = dht.readTemperature();
+  humedad = dht.readHumidity();
   calidad = analogRead(A0);
   Firebase.setFloat(firebaseData,"temperatura",temperatura);
   Firebase.setFloat(firebaseData,"humedad",humedad);
   Firebase.setFloat(firebaseData, "CalidadDeAire",calidad);
+  delay(5000);
 }
