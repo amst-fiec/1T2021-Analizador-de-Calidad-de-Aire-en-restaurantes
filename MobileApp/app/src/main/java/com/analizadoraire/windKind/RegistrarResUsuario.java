afc48429cfb178fc34cb62ecbbb2299bea7cd2c6
carfmeza <carfmeza@espol.edu.ec>
2021-08-13 00:07:15 -0500

    Update RegistrarResUsuario.java

--- MobileApp/app/src/main/java/com/analizadoraire/windKind/RegistrarResUsuario.java
+++ MobileApp/app/src/main/java/com/analizadoraire/windKind/RegistrarResUsuario.java
@@ -2,28 +2,24 @@ package com.analizadoraire.windKind;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class ListDevice extends AppCompatActivity {
-    private TextView listDevice;
+public class RegistrarResUsuario extends ListActivity {
     private ListView list_v1;
     private String nombre_dispositivo[]={};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_device);
-        listDevice =  findViewById(R.id.listDevice);
+        setContentView(R.layout.activity_registrar_res_usuario);
         list_v1 = findViewById(R.id.list_v1);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,R.layout.list_view_devices,nombre_dispositivo);
         list_v1.setAdapter(adapter);
-
-
-
     }
 
 
