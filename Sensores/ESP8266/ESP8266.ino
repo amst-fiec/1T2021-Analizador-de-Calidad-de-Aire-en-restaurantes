2197baeec4502fd9b7252d6369c998b560eac7f0
clairgon <clairgon@espol.edu.ec>
2021-08-12 22:03:43 -0500

    edición ESP8266

--- Sensores/ESP8266/ESP8266.ino
+++ Sensores/ESP8266/ESP8266.ino
@@ -1,46 +1,51 @@
 //ESP8266
 #include <ESP8266WebServer.h>
-#include <ESP8266WiFi.h>
-#include <FirebaseESP8266.h>
-#define FIREBASE_HOST "pruebas-782c9-default-rtdb.firebaseio.com"
-#define FIREBASE_AUTH "kUM0iAZER1fY3T5Q3T0JWGdzXKbDKG3mdtw37nvK"
+//#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+
+#define user = "pruebas-782c9-default-rtdb.firebaseio.com"
+#define password = "kUM0iAZER1fY3T5Q3T0JWGdzXKbDKG3mdtw37nvK"
 #include "DHT.h"
 #define DHTTYPE DHT11
 #define DHTpin 14
+String MacAddress = WiFi.macAddress();
+int PATCH(String payload);
 
 ESP8266WebServer server;
 WiFiClient client;
-FirebaseData firebaseData;
+//FirebaseData firebaseData;
 const char* ssid = "NETLIFE-BARATAU";
 const char* contraseña = "Antonio68";
 float temperatura = 0;
 float humedad = 0;
 float calidad = 0;
 
-DHT dht(DHTpin,DHTTYPE);//iniciar dht11
+DHT dht(DHTpin, DHTTYPE); //iniciar dht11
 
 void setup() {
   dht.begin();
-  WiFi.begin(ssid,contraseña);
+  WiFi.begin(ssid, contraseña);
   Serial.begin(9600);
-  while (WiFi.status()!=WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(500);
-    
+
   }
   Serial.print(" ");
   Serial.print("IP: ");
   Serial.println(WiFi.localIP());
-  Firebase.begin(FIREBASE_HOST,FIREBASE_AUTH);
+  //Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  HTTPClient http;
+  http.begin(client,"https//firestore.googleapis.com/v1/projects/analizadoraire/databases/(default)/documents/sensores/"+MacAddress+"currentDocument.exists=false");
   temperatura = dht.readTemperature();
   humedad = dht.readHumidity();
   calidad = analogRead(A0);
-  Firebase.setFloat(firebaseData,"temperatura",temperatura);
-  Firebase.setFloat(firebaseData,"humedad",humedad);
-  Firebase.setFloat(firebaseData, "CalidadDeAire",calidad);
+  String datos_a_enviar = "";
+  
   delay(5000);
 }
